d85e1b3f56ff113d90b5286f1344d22c9586482a
SHIHAB69 <shihabhowlader873@gmail.com>
2024-11-05 01:17:45 +0600

    method overloading

--- src/BankingSystem/Main.java
+++ src/BankingSystem/Main.java
@@ -1,12 +1,12 @@
 package src.BankingSystem;
 
 public class Main {
-    public void main(String[] args) {
+    public static void main(String[] args)  {
         SavingsAccount account = new SavingsAccount("12345","John Doe",1000.0,2.5);
 
-        System.out.println("Account holder" + account.getAccountHolderName());
-        System.out.println("Account Number" + account.getAccountNumber());
-        System.out.println("Account Balance" + account.getBalance());
+        System.out.println("Account holder " + account.getAccountHolderName());
+        System.out.println("Account Number " + account.getAccountNumber());
+        System.out.println("Account Balance " + account.getBalance());
         account.deposit(500);
         System.out.println("Balance after deposit : $ "+ account.getBalance());
 
