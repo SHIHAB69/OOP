533c2082f454a9d73d671f3ab4121c7eafd763ad
SHIHAB69 <shihabhowlader873@gmail.com>
2024-11-05 01:10:33 +0600

    method overloading

---
+++ src/BankingSystem/SavingsAccount.java
@@ -0,0 +1,34 @@
+package src.BankingSystem;
+
+public class SavingsAccount extends BankAccount{
+    private double interestRate;
+    public SavingsAccount(String accountNumber, String accountHolderName, double balance , double interestRate){
+        super(accountNumber,accountHolderName,balance);
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public  void deposit(double amount){
+        if (amount > 0){
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        }else {
+            System.out.println("Invalid withdrawl amount ");
+        }
+    }
+    @Override
+    public void withdraw(double amount){
+        if (amount > 0 && amount <= balance){
+            balance -= amount;
+            System.out.println("Deposited: $" + amount);
+        }else {
+            System.out.println("Invalid withdrawl amount or insufficient balance");
+        }
+    }
+
+    public void applyInterest(){
+        balance += balance * interestRate;
+        System.out.println("Interest applied: $" + (balance * interestRate));
+    }
+
+}
