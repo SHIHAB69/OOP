9a9d16adda6be3a51e322745fdc18cf14facee55
SHIHAB69 <shihabhowlader873@gmail.com>
2024-11-16 21:29:53 +0600

    Add new classes and fix indentation issue
    Added `Main`, `A` in `properties.access`, `BoxPrice`, and `Circle` classes. Corrected an indentation issue in `Main.java` and adjusted the constructor in `BoxWeight`. Also included a new `.idea/encodings.xml` file for encoding configuration.

---
+++ src/properties/abstractDemo/Daughter.java
@@ -0,0 +1,16 @@
+package src.properties.abstractDemo;
+
+public class Daughter extends Parent{
+
+
+
+    @Override
+    void career() {
+        System.out.println("Im going to be a Doctor");
+    }
+
+    @Override
+    void partner(){
+        System.out.println("I love Daughter");
+    }
+}
