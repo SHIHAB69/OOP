d493393d07ca5f0f40096210cc09dc5054da11f7
SHIHAB69 <shihabhowlader873@gmail.com>
2024-11-17 23:56:50 +0600

    Add new classes and fix indentation issue
    Added `Main`, `A` in `properties.access`, `BoxPrice`, and `Circle` classes. Corrected an indentation issue in `Main.java` and adjusted the constructor in `BoxWeight`. Also included a new `.idea/encodings.xml` file for encoding configuration.

--- src/Test.java
+++ src/Test.java
@@ -1,20 +1,82 @@
 package src;
 
+
 public class Test {
     public static void main(String[] args) {
-        A obj = new A("");
-        System.out.println(obj);
+
+        //create nominee
+        Nominee nominee = new Nominee("Sihab", 203,90);
+
+        //create a committee member
+        CommitteeMember commiteeMember = new CommitteeMember("Sourov",222);
+
+        nominee.showDetails();
+        commiteeMember.showDetails();
+        commiteeMember.casteVote(nominee.id);
+        commiteeMember.evaluateNominee("Good performance this season");
+
+        System.out.println("Nominee Score (Read-only)"+nominee.getNomineeScore());
+
     }
-}
 
-class A{
+   static abstract class Person{
+        protected String name;
+        protected int id;
 
-    final int num = 10;
+        public Person(String name, int id){
+            this.name = name;
+            this.id = id;
+        }
 
+        abstract void showDetails();
+    }
 
-    String name;
-    public A(String name){
-        this.name = name;
+    static class Nominee extends Person{
+        private final int nomineeScore;
+        public Nominee(String name, int id, int nomineeScore){
+            super(name,id);
+            this.nomineeScore = nomineeScore;
+        }
 
+        @Override
+        void showDetails(){
+            System.out.println("Nominee Details");
+            System.out.println("Nominee name: "+ name);
+            System.out.println("Nominee id: "+ id);
+        }
+
+        public int getNomineeScore(){
+            return nomineeScore;
+        }
+    }
+    interface CommiteeActions{
+        void casteVote(int nomineeId);
+        void evaluateNominee(String feedback);
+    }
+
+    static class CommitteeMember extends Person implements CommiteeActions{
+        private String feedback;
+
+        public CommitteeMember(String name, int id){
+            super(name, id);
+        }
+        @Override
+        public void showDetails(){
+            System.out.println("Comittee Member Details");
+            System.out.println("Name: "+name);
+            System.out.println("Id: "+id);
+        }
+
+        @Override
+        public void casteVote(int nomineeId) {
+            System.out.println("Nominee ID casted: "+nomineeId);
+        }
+
+        @Override
+        public void evaluateNominee(String feedback){
+            this.feedback = feedback;
+            System.out.println("Feedback submitted for internal records");
+
+        }
     }
 }
